3e313a0055a0eb84183a8ef4d7d8a64dc8e071e6
myndfyre@valhallalegends.com <myndfyre@valhallalegends.com@95cbdd7c-9a51-0410-b95e-abe9acf5d60f>
2008-07-11 07:52:26 +0000

    Added additional clan support.

---
+++ trunk/development/projects/BNSharp/BattleNet/Clans/ClanFormationEventArgs.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace BNSharp.BattleNet.Clans
+{
+    /// <summary>
+    /// Specifies information provided by Battle.net when the client attempts to form a clan.
+    /// </summary>
+#if !NET_2_ONLY
+    [DataContract]
+#endif
+    public class ClanFormationEventArgs : BaseEventArgs
+    {
+        #region fields
+#if !NET_2_ONLY
+        [DataMember]
+#endif
+        private bool m_success;
+#if !NET_2_ONLY
+        [DataMember]
+#endif
+        private bool m_declined;
+#if !NET_2_ONLY
+        [DataMember]
+#endif
+        private bool m_unavailable;
+#if !NET_2_ONLY
+        [DataMember]
+#endif
+        private string[] m_failedAccounts;
+        #endregion
+
+        /// <summary>
+        /// Creates a new <see>ClanFormationEventArgs</see> indicating success.
+        /// </summary>
+        public ClanFormationEventArgs()
+        {
+            m_success = true;
+            m_failedAccounts = new string[0];
+        }
+
+        /// <summary>
+        /// Creates a new <see>ClanFormationEventArgs</see> indicating failure.
+        /// </summary>
+        /// <param name="declined">Whether the failure is due to users declining.</param>
+        /// <param name="unavailable">Whether the failure is due to users being unavailable.</param>
+        /// <param name="failedAccounts">The accounts that failed.</param>
+        public ClanFormationEventArgs(bool declined, bool unavailable, string[] failedAccounts)
+        {
+            m_declined = declined;
+            m_unavailable = unavailable;
+            m_failedAccounts = failedAccounts;
+        }
+
+        /// <summary>
+        /// Gets whether the invitation succeeded.
+        /// </summary>
+        /// <remarks>
+        /// <para>When this property returns <see langword="true" />, the <see>FailureAccountNames</see> property will return a zero-length array.</para>
+        /// </remarks>
+        public bool Succeeded
+        {
+            get { return m_success; }
+        }
+
+        /// <summary>
+        /// Gets whether the invitation failed because users declined.
+        /// </summary>
+        public bool UsersDeclined
+        {
+            get { return m_declined; }
+        }
+
+        /// <summary>
+        /// Gets whether the invitation failed because users were unavailable.
+        /// </summary>
+        public bool UsersWereUnavailable
+        {
+            get { return m_unavailable; }
+        }
+
+        /// <summary>
+        /// Gets a copy of the account names that failed being invited.
+        /// </summary>
+        public string[] FailureAccountNames
+        {
+            get
+            {
+                string[] copy = new string[m_failedAccounts.Length];
+                Array.Copy(m_failedAccounts, copy, copy.Length);
+                return copy;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Specifies the contract for handlers wishing to listen for clan formation events.
+    /// </summary>
+    /// <param name="sender">The object that originated the event.</param>
+    /// <param name="e">The event arguments.</param>
+    public delegate void ClanFormationEventHandler(object sender, ClanFormationEventArgs e);
+}
--- trunk/development/projects/BNSharp/Net/BattleNetClient.Clan.cs
+++ trunk/development/projects/BNSharp/Net/BattleNetClient.Clan.cs
@@ -48,6 +48,174 @@ namespace BNSharp.Net
             Send(pck);
         }
 
+        /// <summary>
+        /// Begins searching for clan candidates in the channel and friends list, and checks the availability of the specified clan tag.
+        /// </summary>
+        /// <param name="clanTag">The clan tag to check for availability.</param>
+        /// <remarks>
+        /// <para>This method will return immediately, but will cause the <see>ClanCandidatesSearchCompleted</see> event to be fired.  That event does not
+        /// specifically indicate that the proper number of candidates were found, simply that Battle.net responded.  The event arguments sent
+        /// as part of the event indicate the success or failure of the request.</para>
+        /// </remarks>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="clanTag"/> is <see langword="null" />.</exception>
+        public void BeginClanCandidatesSearch(string clanTag)
+        {
+            if (object.ReferenceEquals(clanTag, null))
+                throw new ArgumentNullException("clanTag");
+
+            BncsPacket pck = new BncsPacket((byte)BncsPacketId.ClanFindCandidates);
+            pck.InsertInt32(m_clanCookie++);
+            pck.InsertDwordString(clanTag, 0);
+
+            Send(pck);
+        }
+
+        /// <summary>
+        /// Invites the specified number of users to form a new clan.
+        /// </summary>
+        /// <param name="clanName">The name of the clan to form.</param>
+        /// <param name="clanTag">The tag of the clan to form.</param>
+        /// <param name="usersToInvite">The list of users to invite.  This parameter must be exactly 9 items long.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="clanName"/>, <paramref name="clanTag"/>, 
+        /// <paramref name="userToInvite"/>, or any of the strings in the array of <paramref name="usersToInvite"/>
+        /// is <see langword="null" />.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="usersToInvite"/> is not exactly 9 items long.</exception>
+        public void InviteUsersToNewClan(string clanName, string clanTag, string[] usersToInvite)
+        {
+            if (object.ReferenceEquals(null, clanName)) throw new ArgumentNullException("clanName");
+            if (object.ReferenceEquals(null, clanTag)) throw new ArgumentNullException("clanTag");
+            if (object.ReferenceEquals(null, usersToInvite)) throw new ArgumentNullException("usersToInvite");
+            if (usersToInvite.Length != 9) throw new ArgumentOutOfRangeException("usersToInvite", usersToInvite, "Exactly 9 users may be invited to form a new clan.");
+            for (int i = 0; i < 9; i++)
+                if (object.ReferenceEquals(usersToInvite[i], null)) throw new ArgumentNullException("usersToInvite", "One or more names of users to invite is null.");
+
+            BncsPacket pck = new BncsPacket((byte)BncsPacketId.ClanInviteMultiple);
+            pck.InsertInt32(m_clanCookie++);
+            pck.InsertCString(clanName);
+            pck.InsertDwordString(clanTag, 0);
+            pck.InsertByte(9);
+            for (int i = 0; i < 9; i++)
+            {
+                pck.InsertCString(usersToInvite[i]);
+            }
+
+            Send(pck);
+        }
+
+        // 0x71
+        private void HandleClanInviteMultiple(ParseData pd)
+        {
+            DataReader dr = new DataReader(pd.Data);
+            dr.Seek(4); // cookie
+            ClanResponseCode response = (ClanResponseCode)dr.ReadByte();
+            ClanFormationEventArgs args = null;
+            if (response == ClanResponseCode.Success)
+            {
+                args = new ClanFormationEventArgs();
+            }
+            else
+            {
+                List<string> names = new List<string>();
+                int nextByte = dr.Peek();
+                while (nextByte > 0)
+                {
+                    names.Add(dr.ReadCString());
+                    nextByte = dr.Peek();
+                }
+                args = new ClanFormationEventArgs(response == ClanResponseCode.InvitationDeclined, response == ClanResponseCode.Decline, names.ToArray());
+            }
+            args.EventData = pd;
+
+            OnClanFormationCompleted(args);
+        }
+
+        // 0x72
+        private void HandleClanCreationInvitation(ParseData pd)
+        {
+            DataReader dr = new DataReader(pd.Data);
+            int cookie = dr.ReadInt32();
+            string tag = dr.ReadDwordString(0);
+            string name = dr.ReadCString();
+            string inviter = dr.ReadCString();
+            int inviteeCount = dr.ReadByte();
+            string[] invitees = new string[inviteeCount];
+            for (int i = 0; i < inviteeCount; i++)
+            {
+                invitees[i] = dr.ReadCString();
+            }
+
+            ClanFormationInvitationEventArgs args = new ClanFormationInvitationEventArgs(cookie, tag, name, inviter, invitees) { EventData = pd };
+            OnClanFormationInvitationReceived(args);
+        }
+
+        /// <summary>
+        /// Responds to the invitation to form a new clan.
+        /// </summary>
+        /// <param name="requestID">The request ID, provided by the <see cref="ClanFormationInvitationEventArgs.RequestID">ClanFormationInvitationEventArgs</see>.</param>
+        /// <param name="clanTag">The clan tag.</param>
+        /// <param name="inviter">The user who invited the client to the clan.</param>
+        /// <param name="accept">Whether to accept the invitation.</param>
+        public void RespondToNewClanInvitation(int requestID, string clanTag, string inviter, bool accept)
+        {
+            BncsPacket pck = new BncsPacket((byte)BncsPacketId.ClanCreationInvitation);
+            pck.InsertInt32(requestID);
+            pck.InsertDwordString(clanTag, 0);
+            pck.InsertCString(inviter);
+            pck.InsertByte((byte)(accept ? ClanResponseCode.Accept : ClanResponseCode.Decline));
+
+            Send(pck);
+        }
+
+        /// <summary>
+        /// Disbands the clan to which the client belongs.
+        /// </summary>
+        /// <remarks>
+        /// <para>The client must be the leader of the clan in order to send this command.</para>
+        /// </remarks>
+        public void DisbandClan()
+        {
+            BncsPacket pck = new BncsPacket((byte)BncsPacketId.ClanDisband);
+            pck.InsertInt32(m_clanCookie++);
+            Send(pck);
+        }
+
+        // 0x73
+        private void HandleDisbandClan(ParseData pd)
+        {
+            DataReader dr = new DataReader(pd.Data);
+            dr.Seek(4);
+            ClanResponseCode status = (ClanResponseCode)dr.ReadByte();
+            ClanDisbandEventArgs args = new ClanDisbandEventArgs(status == ClanResponseCode.Success);
+            args.EventData = pd;
+            OnClanDisbandCompleted(args);
+        }
+
+        /// <summary>
+        /// Designates a user as a new clan chieftan (leader).
+        /// </summary>
+        /// <param name="newChieftanName">The name of the new clan chieftan.</param>
+        public void DesignateClanChieftan(string newChieftanName)
+        {
+            if (object.ReferenceEquals(null, newChieftanName))
+                throw new ArgumentNullException("newChieftanName");
+
+            BncsPacket pck = new BncsPacket((byte)BncsPacketId.ClanMakeChieftan);
+            pck.InsertInt32(m_clanCookie++);
+            pck.InsertCString(newChieftanName);
+
+            Send(pck);
+        }
+
+        private void HandleClanMakeChieftan(ParseData pd)
+        {
+            DataReader dr = new DataReader(pd.Data);
+            dr.Seek(4);
+            ClanChieftanChangeResult result = (ClanChieftanChangeResult)dr.ReadByte();
+            ClanChieftanChangeEventArgs args = new ClanChieftanChangeEventArgs(result);
+            args.EventData = pd;
+            OnClanChangeChieftanCompleted(args);
+        }
+
         private void HandleClanQuitNotify(ParseData pd)
         {
             
@@ -66,6 +234,23 @@ namespace BNSharp.Net
         private void HandleClanInvitationResponse(ParseData pd)
         {
 
+        }
+
+        private void HandleClanFindCandidates(ParseData pd)
+        {
+            DataReader dr = new DataReader(pd.Data);
+            dr.Seek(4); // skip the cookie
+            ClanCandidatesSearchStatus status = (ClanCandidatesSearchStatus)dr.ReadByte();
+            int numCandidates = dr.ReadByte();
+            string[] usernames = new string[numCandidates];
+            for (int i = 0; i < numCandidates; i++)
+            {
+                usernames[i] = dr.ReadCString();
+            }
+
+            ClanCandidatesSearchEventArgs args = new ClanCandidatesSearchEventArgs(status, usernames);
+            args.EventData = pd;
+            OnClanCandidatesSearchCompleted(args);
         }
 
         private void HandleClanRankChange(ParseData pd)
