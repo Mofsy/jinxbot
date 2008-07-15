c21b693b701c33634ee48bc3d885b041ead429bb
myndfyre <myndfyre@valhallalegends.com>
2008-07-15 00:08:57 +0000

    Added a demo project: Using custom packet handling to connect to an MCP (realm) server.  This project will become a full-fledged plugin in JinxBot.

---
+++ development/projects/JinxBot.Plugins.McpHandler/RealmFailedEventArgs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BNSharp;
+
+namespace JinxBot.Plugins.McpHandler
+{
+    [Serializable]
+    public class RealmFailedEventArgs : BaseEventArgs
+    {
+        private RealmFailureReason m_reason;
+
+        public RealmFailedEventArgs(RealmFailureReason reason)
+        {
+            m_reason = reason;
+        }
+
+        public RealmFailureReason Reason
+        {
+            get { return m_reason; }
+        }
+    }
+
+    /// <summary>
+    /// Specifies the contract for handlers wishing to listen for realm connection failure events.
+    /// </summary>
+    /// <param name="sender">The object that originated the event.</param>
+    /// <param name="e">The event arguments.</param>
+    public delegate void RealmFailedEventHandler(object sender, RealmFailedEventArgs e);
+}
