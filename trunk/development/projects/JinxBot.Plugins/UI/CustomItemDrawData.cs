cd65a8f1350c00b9d214d6c1ec5af43bf1496950
myndfyre@valhallalegends.com <myndfyre@valhallalegends.com@95cbdd7c-9a51-0410-b95e-abe9acf5d60f>
2008-08-09 06:53:24 +0000

    Was missing the UI folder for some reason...

---
+++ trunk/development/projects/JinxBot.Plugins/UI/CustomItemDrawData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace JinxBot.Plugins.UI
+{
+    public class CustomItemDrawData : DrawItemEventArgs
+    {
+        private object m_item;
+
+        public CustomItemDrawData(DrawItemEventArgs args, object itemToDraw)
+            : base(args.Graphics, args.Font, args.Bounds, args.Index, args.State, args.ForeColor, args.BackColor)
+        {
+            Debug.Assert(!object.ReferenceEquals(null, itemToDraw));
+
+            m_item = itemToDraw;
+        }
+
+        public object Item
+        {
+            get { return m_item; }
+        }
+    }
+}
---
+++ trunk/development/projects/JinxBot.Plugins/UI/CustomMeasurements.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace JinxBot.Plugins.UI
+{
+    public sealed class CustomMeasurements 
+    {
+        private MeasureItemEventArgs m_args;
+        private object m_obj;
+        public CustomMeasurements(MeasureItemEventArgs args, object itemBeingMeasured)
+        {
+            Debug.Assert(!object.ReferenceEquals(args, null));
+            Debug.Assert(!object.ReferenceEquals(itemBeingMeasured, null));
+
+            m_args = args;
+            m_obj = itemBeingMeasured;
+        }
+
+        public Graphics Graphics
+        {
+            get { return m_args.Graphics; }
+        }
+
+        public int Index
+        {
+            get { return m_args.Index; }
+        }
+
+        public int ItemHeight
+        {
+            get { return m_args.ItemHeight; }
+            set { m_args.ItemHeight = value; }
+        }
+
+        public int ItemWidth
+        {
+            get { return m_args.ItemWidth; }
+            set { m_args.ItemWidth = value; }
+        }
+
+        public object Item
+        {
+            get { return m_obj; }
+        }
+    }
+}
