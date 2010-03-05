f896bb5295be5d5f5619f70df720ed550b8816d3
myndfyre <myndfyre@valhallalegends.com>
2010-03-05 06:29:56 +0000

    Going to examine the possibility of making BN# into a more protocol-agnostic API using JinxBot as the plugin host.  Will advise future BN# design.

--- projects/JinxBot.Client.Data/Schema Objects/Tables/dbo.SessionPackets.table.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[SessionPackets]
-(
-[ID] [int] NOT NULL IDENTITY(1, 1),
-[SessionID] [int] NOT NULL,
-[Sent] [bit] NOT NULL,
-[Contents] [varbinary] (2048) NOT NULL
-) ON [PRIMARY]
-
-
